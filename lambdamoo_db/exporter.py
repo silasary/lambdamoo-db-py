23e4518d6d9ed5b1d237f1dca2ff2dfc212bfb0f
Katelyn Gigante <clockwork.singularity@gmail.com>
2023-01-26 15:31:27 +1100

    Case-insensitive please

--- lambdamoo_db/exporter.py
+++ lambdamoo_db/exporter.py
@@ -15,7 +15,7 @@ def converter(x: Any) -> Any:
 
 def sanitize(filename: str) -> str:
     name = re.sub(r"[\*\?\|:;\/\\<>]", "", filename)
-    if name in ["CON", "PRN", "AUX", "NUL", "COM0", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT0", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"]:
+    if name.upper() in ["CON", "PRN", "AUX", "NUL", "COM0", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT0", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"]:
         return ""
     return name
 
