57028990b18db988d05770b18d2ae569db230e0b
Katelyn Gigante <clockwork.singularity@gmail.com>
2023-01-26 16:26:20 +1100

    Black formatting

--- lambdamoo_db/cli.py
+++ lambdamoo_db/cli.py
@@ -2,9 +2,10 @@ import click
 from .exporter import to_moo_files
 from .reader import load
 
+
 @click.command()
-@click.argument('dbfile')
-@click.argument('dir')
+@click.argument("dbfile")
+@click.argument("dir")
 def moodb2flat(dbfile: str, dir: str) -> None:
     db = load(dbfile)
     to_moo_files(db, dir, True)
--- lambdamoo_db/exporter.py
+++ lambdamoo_db/exporter.py
@@ -8,6 +8,34 @@ import cattrs
 from lambdamoo_db.database import ObjNum, WaifReference, MooDatabase
 
 
+ILLEGAL_NAMES = [
+    "CON",
+    "PRN",
+    "AUX",
+    "NUL",
+    "COM0",
+    "COM1",
+    "COM2",
+    "COM3",
+    "COM4",
+    "COM5",
+    "COM6",
+    "COM7",
+    "COM8",
+    "COM9",
+    "LPT0",
+    "LPT1",
+    "LPT2",
+    "LPT3",
+    "LPT4",
+    "LPT5",
+    "LPT6",
+    "LPT7",
+    "LPT8",
+    "LPT9",
+]
+
+
 def converter(x: Any) -> Any:
     if isinstance(x, WaifReference):
         return f"WAIF({x.index})"
@@ -15,7 +43,7 @@ def converter(x: Any) -> Any:
 
 def sanitize(filename: str) -> str:
     name = re.sub(r"[\*\?\|:;\/\\<>]", "", filename)
-    if name.upper() in ["CON", "PRN", "AUX", "NUL", "COM0", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT0", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"]:
+    if name.upper() in ILLEGAL_NAMES:
         return ""
     return name
 
